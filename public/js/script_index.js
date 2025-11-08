e9919c2535ed9653f00300494ddd71f4f8c2775e
Xtreme-654 <xtremedosamba@gmail.com>
2025-11-08 13:09:33 -0300

    Pagina login feita

--- public/js/script_index.js
+++ public/js/script_index.js
@@ -1,36 +1,37 @@
 var carrocel_index = [
-`<img src="./assets/img_carrocel-0.png" alt="">`,
-`<img src="./assets/img_carrocel-1.png" alt="">`,
-`<img src="./assets/img_carrocel-2.png" alt="">`
+  `<img src="./assets/img_carrocel-0.png" alt="">`,
+  `<img src="./assets/img_carrocel-1.png" alt="">`,
+  `<img src="./assets/img_carrocel-2.png" alt="">`
 ]
-var num_carrocel = 1;
+var num_carrocel = 0;
 var carrocel_max = carrocel_index.length - 1;
-function carrocel(){
-  
-  if(num_carrocel < carrocel_index.length){
+function carrocel() {
+
+  if (num_carrocel < carrocel_index.length) {
     imagem_meio.innerHTML = `${carrocel_index[num_carrocel]}`
-  }else if(num_carrocel >= carrocel_index.length){
-    num_carrocel == 0;
-  }else if(num_carrocel <0){
-    num_carrocel == 0;
+  } else if (num_carrocel >= carrocel_index.length) {
+    num_carrocel = 0;
+  } else if (num_carrocel < 0) {
+    num_carrocel = 0;
   }
   num_carrocel++
 }
 
-function go(){
+function go() {
   num_carrocel++
-  if(num_carrocel >= carrocel_index.length){
-    num_carrocel == 0;
+  if (num_carrocel >= carrocel_index.length) {
+    num_carrocel = 0;
   }
   carrocel();
 }
 
-function back(){
-  num_carrocel--
-  if(num_carrocel < 0){
-    num_carrocel == carrocel_max;
+function back() {
+ 
+  imagem_meio.innerHTML = `${carrocel_index[num_carrocel--]}`
+  if (num_carrocel < 0) {
+     imagem_meio.innerHTML = `${carrocel_index[carrocel_max]}`
   }
   carrocel();
 }
 
-setInterval(carrocel, 5000);
+setInterval(go, 5000);
